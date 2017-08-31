3f70566d8e8dd1aba972dbc2f4e036618cfb4e53
Otavio Angeli <otavio.miguel.angeli@gmail.com>
2017-08-31 15:10:52 -0300

    Criado layout base para lista de séries, retirado icone 'Sair' do menu lateral

---
+++ app/src/main/java/uniandrade/br/edu/com/seriadosbrasileiros/api/SeriesResults.java
@@ -0,0 +1,267 @@
+package uniandrade.br.edu.com.seriadosbrasileiros;
+
+import java.util.List;
+
+/**
+ * Created by oangeli on 30/08/17.
+ */
+
+public class SeriesResults {
+
+    private String created_by;
+    private String description;
+    private int favorite_count;
+    private String id;
+    private int item_count;
+    private String iso_639_1;
+    private String name;
+    private Object poster_path;
+    private List<ItemsBean> items;
+
+    public String getCreated_by() {
+        return created_by;
+    }
+
+    public void setCreated_by(String created_by) {
+        this.created_by = created_by;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getFavorite_count() {
+        return favorite_count;
+    }
+
+    public void setFavorite_count(int favorite_count) {
+        this.favorite_count = favorite_count;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getItem_count() {
+        return item_count;
+    }
+
+    public void setItem_count(int item_count) {
+        this.item_count = item_count;
+    }
+
+    public String getIso_639_1() {
+        return iso_639_1;
+    }
+
+    public void setIso_639_1(String iso_639_1) {
+        this.iso_639_1 = iso_639_1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Object getPoster_path() {
+        return poster_path;
+    }
+
+    public void setPoster_path(Object poster_path) {
+        this.poster_path = poster_path;
+    }
+
+    public List<ItemsBean> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemsBean> items) {
+        this.items = items;
+    }
+
+    public static class ItemsBean {
+
+        private String original_name;
+        private int id;
+        private String media_type;
+        private String name;
+        private int vote_count;
+        private double vote_average;
+        private String poster_path;
+        private String first_air_date;
+        private double popularity;
+        private String original_language;
+        private String backdrop_path;
+        private String overview;
+        private boolean video;
+        private String title;
+        private String original_title;
+        private boolean adult;
+        private String release_date;
+        private List<?> genre_ids;
+        private List<?> origin_country;
+
+        public String getOriginal_name() {
+            return original_name;
+        }
+
+        public void setOriginal_name(String original_name) {
+            this.original_name = original_name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getMedia_type() {
+            return media_type;
+        }
+
+        public void setMedia_type(String media_type) {
+            this.media_type = media_type;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getVote_count() {
+            return vote_count;
+        }
+
+        public void setVote_count(int vote_count) {
+            this.vote_count = vote_count;
+        }
+
+        public double getVote_average() {
+            return vote_average;
+        }
+
+        public void setVote_average(double vote_average) {
+            this.vote_average = vote_average;
+        }
+
+        public String getPoster_path() {
+            return poster_path;
+        }
+
+        public void setPoster_path(String poster_path) {
+            this.poster_path = poster_path;
+        }
+
+        public String getFirst_air_date() {
+            return first_air_date;
+        }
+
+        public void setFirst_air_date(String first_air_date) {
+            this.first_air_date = first_air_date;
+        }
+
+        public double getPopularity() {
+            return popularity;
+        }
+
+        public void setPopularity(double popularity) {
+            this.popularity = popularity;
+        }
+
+        public String getOriginal_language() {
+            return original_language;
+        }
+
+        public void setOriginal_language(String original_language) {
+            this.original_language = original_language;
+        }
+
+        public String getBackdrop_path() {
+            return backdrop_path;
+        }
+
+        public void setBackdrop_path(String backdrop_path) {
+            this.backdrop_path = backdrop_path;
+        }
+
+        public String getOverview() {
+            return overview;
+        }
+
+        public void setOverview(String overview) {
+            this.overview = overview;
+        }
+
+        public boolean isVideo() {
+            return video;
+        }
+
+        public void setVideo(boolean video) {
+            this.video = video;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getOriginal_title() {
+            return original_title;
+        }
+
+        public void setOriginal_title(String original_title) {
+            this.original_title = original_title;
+        }
+
+        public boolean isAdult() {
+            return adult;
+        }
+
+        public void setAdult(boolean adult) {
+            this.adult = adult;
+        }
+
+        public String getRelease_date() {
+            return release_date;
+        }
+
+        public void setRelease_date(String release_date) {
+            this.release_date = release_date;
+        }
+
+        public List<?> getGenre_ids() {
+            return genre_ids;
+        }
+
+        public void setGenre_ids(List<?> genre_ids) {
+            this.genre_ids = genre_ids;
+        }
+
+        public List<?> getOrigin_country() {
+            return origin_country;
+        }
+
+        public void setOrigin_country(List<?> origin_country) {
+            this.origin_country = origin_country;
+        }
+    }
+}
